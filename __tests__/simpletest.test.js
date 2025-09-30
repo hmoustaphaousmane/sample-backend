455ff829309b9b675bac78ed7b665a9453f888c0
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-09-30 13:58:56 +0100

    test: add user verification and login test

--- __tests__/simpletest.test.js
+++ __tests__/simpletest.test.js
@@ -1,11 +1,17 @@
 const request = require("supertest");
 
+// Increase Jest timeout (mongoose and bcrypt operations can might take more than 5s)
+jest.setTimeout(20000); // 20s for all tests in this file
+
 const httpServer = require("../app");
 const userModel = require("../schema/user");
 const otpModel = require("../schema/otp");
 const mongoose = require("mongoose");
 
-const email = "hmoustaphaousmane@gmail.com";
+const fullName = "Hassane M. O.";
+const email = `email${Date.now()}@gmail.com`;
+let otpDetails;
+let token = "";
 
 // beforeEach(); // Runs before each test
 // afterEach(); // Runs after each test
@@ -16,35 +22,67 @@ beforeAll(async () => {
   await otpModel.deleteMany({});
 });
 
-// Runs agter all tests
+// Runs after all tests
 afterAll(async () => {
   httpServer.close(); // Halt server
 
-  await mongoose.disconnect(); // Disconnect mongosse from database
+  await mongoose.disconnect(); // Disconnect mongoose from database
 });
 
-describe("Test for register and login features", () => {
+describe("Test for register, verify and login features", () => {
   test("Register with full name, email, password and role", async () => {
-    const response = await request(httpServer).post("/auth/register").send({
-      fullName: "Hassane M. O.",
-      email,
-      password: "whatever",
-      role: "customer",
-    });
+    const response = await request(httpServer)
+      .post("/auth/register")
+      .set("Content-Type", "application/json") // Not neccessary if sending a json object
+      .send({
+        fullName,
+        email,
+        password: "whatever",
+        role: "customer",
+      });
     // .set("Authorization", `Bearer ${token}`);
 
     // console.log(response);
 
     const userDetails = await userModel.findOne({ email });
-    const otpDetails = await otpModel.findOne({
+    // console.log("User Details:", userDetails);
+
+    otpDetails = await otpModel.findOne({
       userId: userDetails._id,
       purpose: "verify-email",
       otpToken: response.body.otpToken,
     });
-    // Todo: Check OTP: optModel.findOne(){} and comparer with response.body.otpToken
+    // console.log("OTP Details:", otpDetails);
+
     expect(response.status).toBe(201);
     expect(response.body.message).toBe("User created successfully");
     expect(response.body.otpToken).toBe(otpDetails.otpToken);
     expect(response.body.purpose).toBe("verify-email");
+  });
+
+  test("Verify customer's email", async () => {
+    const response = await request(httpServer).patch("/auth/verify").send({
+      otp: otpDetails.otp,
+      otpToken: otpDetails.otpToken,
+      purpose: otpDetails.purpose,
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("User email verified successfully");
+  });
+
+  test("should Customer login with email and password", async () => {
+    const response = await request(httpServer).post("/auth/login").send({
+      email,
+      password: "whatever",
+    });
+
+    token = response.body.token;
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Login successful");
+    expect(response.body.userDetail.fullName).toBe(fullName);
+    expect(response.body.userDetail.email).toBe(email);
+    expect(response.body.token).toBeTruthy();
   });
 });
